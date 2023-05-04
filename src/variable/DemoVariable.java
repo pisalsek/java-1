503933c78429421ff0bd550c61ad98f5834602be
pisalsek <pisalsek99@gmail.com>
2023-05-04 13:47:41 +0700

    A

---
+++ src/variable/DemoVariable.java
@@ -0,0 +1,35 @@
+package variable;
+
+import java.util.Arrays;
+
+public class DemoVariable {
+
+    public static void main(String[] args) {
+        boolean a=true;
+            System.out.println("a = " + a);
+
+        byte age = 17;
+            System.out.println("age = " + age);
+        short number = 127;
+            System.out.println("number = " + number);
+        int total=1000;
+            System.out.println("total = " + total);
+        long peopleOfCambodai=1839090L;
+            System.out.println("peopleOfCambodai = " + peopleOfCambodai);
+
+        float totalPrice=122.33F;
+            System.out.println("totalPrice = " + totalPrice);
+        double salary = 1239.450;
+            System.out.println("salary = " + salary);
+
+        char gender='M';
+            System.out.println("gender = " + gender);
+
+
+
+
+
+
+
+    }
+}
