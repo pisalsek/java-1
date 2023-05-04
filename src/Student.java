4128c9b3617998913eaf85ede11851eeffd5a35a
pisalsek <pisalsek99@gmail.com>
2023-05-04 13:17:59 +0700

    j

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+import java.awt.geom.Arc2D;
+
+public class Student {
+    private static byte truel;
+
+    public static void main(String[] args) {
+        float x = 99.5F;
+        double salary =555.9900;
+
+        long b = 12l;
+        int age =18;
+        byte c = 50;
+        short d= 10000;
+        
+        String name ="pisal";
+        char gender ='M';
+
+        boolean are =false;
+
+            System.out.println(" Value of X is : "+x);
+            System.out.println("Male is : "+gender);
+            System.out.println("My age is : "+age);
+            System.out.println("My salary is : "+salary);
+            System.out.println("boolean is : "+are);
+            System.out.println(b);
+            System.out.println("My name is : "+name);
+            System.out.println(c);
+
+    }
+}
